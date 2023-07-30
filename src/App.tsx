e2dfabd29705094f0c0c1c5b2b17e50c71fee42b
unknown <asskarnwin@gmail.com>
2023-07-30 15:04:23 +0700

    add chakraui + wallet adapter

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, useMemo, FC, useEffect } from 'react'
+import './App.css'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import "@solana/wallet-adapter-react-ui/styles.css"
+
+function App() {
+
+  return (
+    <WalleAdapterContext>
+      <div className='App'>
+        <WalletMultiButton/>
+      </div>
+    </WalleAdapterContext>
+  )
+}
+
+const WalleAdapterContext: FC<{ children: ReactNode }> = (
+  { children }: { children: ReactNode }
+) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new UnsafeBurnerWalletAdapter(),
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+);
+}
+
+export default App
