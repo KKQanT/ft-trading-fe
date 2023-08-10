d96e97209fbc2f7dbfa2824613d5f1442ce5d104
unknown <asskarnwin@gmail.com>
2023-08-10 21:45:32 +0700

    fix buffer not defined on prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,14 +9,15 @@ export default defineConfig({
   define: {
     'process.env.ANCHOR_BROWSER': true
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis"
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({buffer: true})
-      ]
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({Buffer:["buffer", "Buffer"]})],
     }
   }
+  
 })
